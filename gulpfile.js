5e178662387cece0b64040df98bf5bb96c9f1fb8
litianxing <837727384@qq.com>
2022-10-29 15:04:59 +0800

    打包优化

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,8 @@
 const gulp = require('gulp')
 const rollup = require('rollup')
 const ts = require('gulp-typescript');
-const rename = require("gulp-rename");
-const uglify = require('gulp-uglify-es').default;
 const tsProject = ts.createProject('tsconfig.json', { declaration: true });
-const resolve = require('rollup-plugin-node-resolve')
 const clean = require('gulp-clean')
-
-const onwarn = warning => {
-    // Silence circular dependency warning for moment package
-    if (warning.code === 'CIRCULAR_DEPENDENCY')
-        return
-
-    console.warn(`(!) ${warning.message}`)
-}
 
 gulp.task('buildJs', () => {
     return tsProject.src()
@@ -32,26 +21,18 @@ gulp.task('cleanJs', () => {
 gulp.task("rollup", async function() {
     let config = {
         input: "build/FairyGUI.js",
-        external: ['three'],
         output: {
             file: 'FairyGUI.js',
             format: 'esm',
             extend: true,
-            name: 'fgui',
-            globals: { three: 'three' }
+            name: 'fgui'
         },
         plugins: [
-            resolve()
+            // resolve()
         ]
     };
     const subTask2 = await rollup.rollup(config);
     await subTask2.write(config);
-});
-
-gulp.task("uglify", function() {
-    return gulp.src("FairyGUI.js")
-        .pipe(uglify( /* options */ ))
-        .pipe(gulp.dest("build/"));
 });
 
 gulp.task("movejs", function() {
@@ -69,11 +50,6 @@ gulp.task("movetypes", function() {
         .pipe(gulp.dest('../panorama-fgui-types/types'));
 });
 
-gulp.task("finalclean", function() {
-    return gulp.src("FairyGUI.js")
-        .pipe(clean('FairyGUI.js'));
-});
-
 gulp.task('build', gulp.series(
     gulp.parallel('buildJs'),
     gulp.parallel('rollup'),
@@ -81,5 +57,4 @@ gulp.task('build', gulp.series(
     gulp.parallel('move'),
     gulp.parallel('movejs'),
     gulp.parallel('movetypes'),
-    gulp.parallel('finalclean'),
 ))
